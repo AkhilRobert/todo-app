d43a6eb82b9c752d2826fbaae30a8f72f099b209
Akhil Robert <akhil@Akhils-MacBook-Pro.local>
2021-04-15 14:45:45 +0530

    declared all routes for backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import express from 'express';
+import todoRoutes from './routes/todo';
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use('/api/todo/', todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server started at the port ${PORT}`);
+});
