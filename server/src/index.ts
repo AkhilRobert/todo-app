29474ff412160809d77e7b70bfb70e9f4c19e6a7
Akhil Robert <akhil@Akhils-MacBook-Pro.local>
2021-04-16 13:32:06 +0530

    fixed cors issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import todoRoutes from './routes/todo.route';
 import './db/db';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use('/api/todo/', todoRoutes);
 
 app.listen(PORT, () => {
